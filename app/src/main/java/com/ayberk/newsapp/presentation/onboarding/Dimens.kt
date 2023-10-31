6d83c114ea41a0029ee09ef779e803f4003b05d9
ayberktmn <ayberktemin@gmail.com>
2023-10-31 17:27:57 +0300

    boardingScreen done

---
+++ app/src/main/java/com/ayberk/newsapp/presentation/onboarding/Dimens.kt
@@ -0,0 +1,13 @@
+package com.ayberk.newsapp.presentation.onboarding
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    val MediumPadding1 = 24.dp
+    val MediumPadding2 = 30.dp
+    val MediumPadding3 = 10.dp
+    val IndicatorSize = 14.dp
+
+
+}
---
+++ app/src/main/java/com/ayberk/newsapp/presentation/onboarding/components/PageIndicator.kt
@@ -0,0 +1,36 @@
+package com.ayberk.newsapp.presentation.onboarding.components
+
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import com.ayberk.newsapp.presentation.onboarding.Dimens.IndicatorSize
+
+@Composable
+fun PageIndicator(
+modifier: Modifier= Modifier,
+pageSize:Int,
+selectedPage:Int,
+selectedColor:Color = MaterialTheme.colorScheme.primary,
+unselectedColor:Color = Color.Blue
+
+){
+Row(modifier = modifier, horizontalArrangement = Arrangement.SpaceBetween) {
+    repeat(pageSize){ page ->
+        Box (modifier = modifier
+            .size(IndicatorSize)
+            .clip(CircleShape)
+            .background(color = if (page == selectedPage) selectedColor else unselectedColor)
+        )
+        Log.e("a","aasg")
+    }
+  }
+}

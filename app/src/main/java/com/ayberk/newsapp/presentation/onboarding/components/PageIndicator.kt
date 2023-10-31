32b9c9adfba88218c6233bbe07b41ee4107ac0ff
ayberktmn <ayberktemin@gmail.com>
2023-10-31 18:16:38 +0300

    boardingScreen done

--- app/src/main/java/com/ayberk/newsapp/presentation/onboarding/components/PageIndicator.kt
+++ app/src/main/java/com/ayberk/newsapp/presentation/onboarding/components/PageIndicator.kt
@@ -30,7 +30,7 @@ Row(modifier = modifier, horizontalArrangement = Arrangement.SpaceBetween) {
             .clip(CircleShape)
             .background(color = if (page == selectedPage) selectedColor else unselectedColor)
         )
-      Log.e("a","asgadadgadhad")
+
     }
   }
 }
